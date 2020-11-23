acf1601d902807fb36ec6ec8cb778e82d0177266
Kevin Dias <kevin.dias.20160@gmail.com>
2020-11-23 08:26:40 -0300

    first commit

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChatIntro.css';
+
+function ChatIntro() {
+    return (
+        <div className="chatIntro">
+            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt="" />
+            <h1>Mantenha seu telefone conectado</h1>
+            <h2>O WhatsApp conecta seu telefone para sincronizar suas mensagens. <br/> Para reduzir o uso de dados, conecte seu telefone a uma rede Wi-Fi.</h2>
+        </div>
+    )
+}
+
+export default ChatIntro
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default  {
+    apiKey: "AIzaSyAITOE9TzIE6G2XogpQKER7XCaH1uUojco",
+    authDomain: "whatsappclonedias.firebaseapp.com",
+    databaseURL: "https://whatsappclonedias.firebaseio.com",
+    projectId: "whatsappclonedias",
+    storageBucket: "whatsappclonedias.appspot.com",
+    messagingSenderId: "990633342169",
+    appId: "1:990633342169:web:5be6980256949d32817ac8"
+  };
+
